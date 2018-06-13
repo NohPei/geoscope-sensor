f7a74ff10ef04779fbb8e3cc124f3c0c67aa8402
LIURLN <liurln38@gmail.com>
2018-06-13 13:34:35 +0700

    Add project files.

---
+++ geoscope/geoscope.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		geoscope.ino
+ Created:	6/13/2018 1:34:31 PM
+ Author:	Sripong
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
