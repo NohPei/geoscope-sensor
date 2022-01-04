2a307fa33195d0c13f3c4fca45d718533beb415b
Jesse R Codling <jclds139@gmail.com>
2022-01-03 20:28:42 -0500

    ESP8266 TSF Timer Registers
    After some header digging and experimentation, we've found the registers
    that correspond to the STA-mode TSF timer and copied them to Network.h
    from the RTOS SDK's headers

--- geoscope/Network.h
+++ geoscope/Network.h
@@ -12,6 +12,11 @@
 #include "main.h"
 #include <IPAddress.h>
 
+//TSF Register Addresses - imported from eagle_soc.h in RTOS SDK
+// 	original: https://github.com/espressif/ESP8266_RTOS_SDK/blob/v3.4/components/esp8266/include/esp8266/eagle_soc.h:149:150
+#define ESP8266_WDEVTSF0_TIME_LO 0x3ff21004
+#define ESP8266_WDEVTSF0_TIME_HI 0x3ff21008
+
 extern char SSID[32], PASSWORD[32];
 extern IPAddress  GATEWAY_IP, NETMASK, GEOSCOPE_IP, DNS;
 
