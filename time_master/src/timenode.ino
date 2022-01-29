0f8d87b7e64098b5e56336fe9f98ec9134caa2e7
Jesse R Codling <jclds139@gmail.com>
2022-01-29 15:44:44 -0500

    Time Master RBIS and MQTT glue

--- time_master/src/timenode.ino
+++ time_master/src/timenode.ino
@@ -3,37 +3,84 @@
 #include<ClockControl.h>
 #include<cstdlib>
 #include<esp_undocumented.h>
+#include<ESP8266Wifi.h>
+#include<PubSubClient.h>
+#include<RBIS.h>
 
 packetio::SLIPStream timeStream = packetio::SLIPStream(Serial);
 packetio::PacketListener timeListener = packetio::PacketListener(timeStream);
 LSLRClockController realTime();
-volatile timestamp_t last_time_from_server = 0;
-volatile ts_pair_t* new_sample = NULL;
+RBIS_UDP timeSyncClient();
+volatile timestamp_t next_pps_time = 0;
+volatile struct ready_sample {
+	ts_pair_t sample;
+	bool ready;
+} new_sample;
+
+const char MQTT_BROKER_IP[] = "10.244.1.1";
+const int MQTT_BROKER_PORT = 18884;
+const char MQTT_CLIENT_ID[] = "RBIS_Master";
+const char WIFI_SSID[] = "The Promised LAN";
+const char WIFI_PSK[] = "GoBucks!";
+
+#define PPS_INTERRUPT digitalPinToInterrupt(D0)
 
 void IRAM_CACHE_ATTR pps_isr_handler() {
 	timestamp_t system_ts = ESP_WDEV_TIMESTAMP();
-	if (last_time_from_server != 0) {
-		timestamp_t server_time = last_time_from_server - (last_time_from_server % 1000000) + 1000000;
-		last_time_from_server = 0;
-		//disable my interrupt here, too
-		new_sample = new ts_pair_t();
-		new_sample->local = system_ts;
-		new_sample->target = server_time;
+	if (next_pps_time != 0) {
+		next_pps_time = 0;
+
+		//disable my interrupt, don't need to go again
+		detachInterrupt(PPS_INTERRUPT)
+
+
+		new_sample.sample.local = system_ts;
+		new_sample.sample.target = next_pps_time;
+		new_sample.ready = true;
 	}
 }
+
+WifiClient dataClient();
+PubSubClient mqtt(dataClient);
+
+void mqttSetup() {
+	mqtt.setServer(MQTT_BROKER_IP, MQTT_BROKER_PORT);
+	while (!mqtt.connect(MQTT_CLIENT_ID)) {
+		yield();
+	}
+
+}
+
+void mqttSendFUp(byte[] msg, size_t len) {
+	mqtt.publish("geoscope/rbis", msg, len);
+}
+
+
 
 
 void clockUpdate(byte* packet, size_t len) {
 	packet[len] = '\0'; //force a null terminator after the end of the packet
-	last_time_from_server = strtoull((char*) packet, NULL, 16);
+	next_pps_time = strtoull((char*) packet, NULL, 16);
+	next_pps_time -= (next_pps_time % 1000000); //eliminate the subsecond values
+	next_pps_time += 1000000; //next pulse comes at the start of the next second
 
 	//arm the PPS interrupt now
+	attachInterrupt(PPS_INTERRUPT, pps_isr_handler, RISING);
+		//trigger the real time sample on the next PPS rising edge
 	
 }
 
 void setup() {
-	//set up RBIS as master
-	//set up an LSLRClockController to manage our local time
+	new_sample.ready = false;
+
+	WiFi.setMode(WIFI_STA);
+	WiFi.setPhyMode(WIFI_PHY_MODE_11N);
+	WiFi.begin(WIFI_SSID, WIFI_PSK);
+
+
+	mqttSetup();
+	timeSyncClient.masterSetCb(&mqttSendFUp);
+	timeSyncClient.masterSetConversion(&(realTime.convertTime));
 
 	Serial.begin(256000);
 	timeListener.onMessage(&clockUpdate);
@@ -45,10 +92,10 @@ void setup() {
 void loop() {
 	timeListener.handle();
 
-	if (new_sample) { //check to see if we have a new timestamp pair ready
-		realTime.addTimeSample(new_sample.local, new_sample.target); //if so, tell the clock about it
-		delete new_sample; //and clear it so we're ready for the next one
-		new_sample = NULL;
+
+	if (new_sample.ready) { //check to see if we have a new timestamp pair ready
+		realTime.addTimeSample(new_sample.sample.local, new_sample.sample.target); //if so, tell the clock about it
+		new_sample.ready = NULL;
 	}
 
 }
