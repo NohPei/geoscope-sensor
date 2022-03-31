a1dbdda1952bc597817ac6e936702e4ca921592f
Jesse R Codling <jclds139@gmail.com>
2022-03-31 16:53:58 -0400

    Setup to allow for new ESP-C3-12F modules

---
+++ geoscope/esp_undocumented.h
@@ -0,0 +1,30 @@
+#if _ESP_UNDOCUMENTED_H_
+#define _ESP_UNDOCUMENTED_H_
+
+#include<stdint.h>
+
+#if defined(EPS8266)
+
+#define WDEVTSF0_TIME_LO 			0x3ff21004
+#define WDEVTSF0_TIME_HI 			0x3ff21008
+#define WDEVTSFSW0_LO 				0x3ff21018
+#define WDEVTSFSW0_HI 				0x3ff2101C
+#define WDEVTSF0_TIMER_LO 		0x3ff2109c
+#define WDEVTSF0_TIMER_HI 		0x3ff210a0
+#define WDEVTSF0_TIMER_ENA 		0x3ff21098
+#define WDEVTSF0_TIM_EN_MASK 	0x80000000	
+#define WDEV_COUNT_REG 				0x3ff21004
+
+#define ESP_WDEV_TIMESTAMP() 		(* (volatile int64_t*) WDEVTSF0_TIME_LO)
+
+extern int wDev_MacTimSetFunc(void (*handle)(void));
+
+#elif defined(ESP32)
+
+#include <esp_wifi.h>
+#define ESP_WDEV_TIMESTAMP() 		esp_wifi_get_tsf_time((wifi_interface_t)0)
+
+#endif
+
+
+#endif //define _ESP_UNDOCUMENTED_H_
