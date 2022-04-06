d374a8840e1a7b098790901c921fbc79ccc88f50
Jesse R Codling <jclds139@gmail.com>
2022-04-05 21:48:45 -0400

    Separate `esp_undocumented` into its own file
    Uses the time node firmware from geoscope-gateway as a "library" so
    there's only one copy of it.

--- geoscope/Network.h
+++ geoscope/Network.h
@@ -11,33 +11,11 @@
 
 #include "main.h"
 #include <IPAddress.h>
+#include <esp_undocumented.h>
 
 extern char SSID[32], PASSWORD[32];
 extern IPAddress  GATEWAY_IP, NETMASK, GEOSCOPE_IP, DNS;
 
-# ifdef ESP8266
-//Undocumented ESP8266 registers
-#define WDEVTSF0_TIME_LO 			0x3ff21004
-#define WDEVTSF0_TIME_HI 			0x3ff21008
-#define WDEVTSFSW0_LO 				0x3ff21018
-#define WDEVTSFSW0_HI 				0x3ff2101C
-#define WDEVTSF0_TIMER_LO 		0x3ff2109c
-#define WDEVTSF0_TIMER_HI 		0x3ff210a0
-#define WDEVTSF0_TIMER_ENA 		0x3ff21098
-#define WDEVTSF0_TIM_EN_MASK 	0x80000000	
-#define WDEV_COUNT_REG 				0x3ff21004
-
-#define ESP_WDEV_TIMESTAMP() (* (volatile uint64_t*) WDEVTSF0_TIME_LO)
-
-extern int wDev_MacTimSetFunc(void (*handle)(void));
-# endif
-
-# ifdef ESP32
-
-#include <esp_wifi.h>
-#define ESP_WDEV_TIMESTAMP() esp_wifi_get_tsf_time((wifi_interface_t)0)
-
-# endif
 
 void networkSetup();
 void wifiSetup();
